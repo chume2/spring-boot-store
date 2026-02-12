73f8dc36ea99846a466d97696b0ea8545047ff37
Charlie Hume <charliehume0806@gmail.com>
2026-02-11 22:28:46 -0500

    Refactored AuthController and AuthService

--- src/main/java/com/codewithmosh/store/auth/AuthController.java
+++ src/main/java/com/codewithmosh/store/auth/AuthController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.codewithmosh.store.users.UserDto;
 import com.codewithmosh.store.users.UserMapper;
-import com.codewithmosh.store.users.UserRepository;
-
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
@@ -15,9 +13,7 @@ import lombok.AllArgsConstructor;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,53 +25,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-    private final AuthenticationManager authenticationManager;
-    private final JwtService jwtService;
     private final JwtConfig jwtConfig;
-    private final UserRepository userRepository;
     private final UserMapper userMapper;
     private final AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity<JwtResponse> login(
+    public JwtResponse login(
         @Valid @RequestBody LoginRequest request,
         HttpServletResponse response
     ) {
-        authenticationManager.authenticate(
-            new UsernamePasswordAuthenticationToken(
-                request.getEmail(), 
-                request.getPassword()
-            )
-        );
+        var loginResult = authService.login(request);
 
-        var user = userRepository.findByEmail(request.getEmail()).orElseThrow();
-        var accessToken = jwtService.generateAccessToken(user);
-        var refreshToken = jwtService.generateRefreshToken(user);
-
-        var cookie = new Cookie("refreshToken", refreshToken.toString());
+        var cookie = new Cookie("refreshToken", loginResult.getRefreshToken().toString());
         cookie.setHttpOnly(true);
-        cookie.setPath("/");
-        cookie.setMaxAge(jwtConfig.getRefreshTokenExpiration()); // 7 days
+        cookie.setPath("/auth/refresh");
+        cookie.setMaxAge(jwtConfig.getRefreshTokenExpiration());
         cookie.setSecure(true);
         response.addCookie(cookie);
 
-        return ResponseEntity.ok(new JwtResponse(accessToken.toString()));
+        return new JwtResponse(loginResult.getAccessToken().toString());
     }
 
     @PostMapping("/refresh")
-    public ResponseEntity<JwtResponse> refresh(
+    public JwtResponse refresh(
         @CookieValue(value = "refreshToken") String refreshToken
     ) {
-        var jwt = jwtService.parseToken(refreshToken);
-
-        if (jwt == null || jwt.isExpired()) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-
-        var user = userRepository.findById(jwt.getUserId()).orElseThrow();
-        var accessToken = jwtService.generateAccessToken(user);
-
-        return ResponseEntity.ok(new JwtResponse(accessToken.toString()));
+        var accessToken = authService.refreshAccessToken(refreshToken);
+        return new JwtResponse(accessToken.toString());
     }
 
     @GetMapping("/me")
--- src/main/java/com/codewithmosh/store/auth/AuthService.java
+++ src/main/java/com/codewithmosh/store/auth/AuthService.java
@@ -1,5 +1,8 @@
 package com.codewithmosh.store.auth;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +15,33 @@ import lombok.AllArgsConstructor;
 @Service
 public class AuthService {
     private final UserRepository userRepository;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+
+    public LoginResponse login(LoginRequest request) {
+        authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                request.getEmail(), 
+                request.getPassword()
+            )
+        );
+
+        var user = userRepository.findByEmail(request.getEmail()).orElseThrow();
+        var accessToken = jwtService.generateAccessToken(user);
+        var refreshToken = jwtService.generateRefreshToken(user);
+
+        return new LoginResponse(accessToken, refreshToken);
+    }
+
+    public Jwt refreshAccessToken(String refreshToken) {
+        var jwt = jwtService.parseToken(refreshToken);
+        if (jwt == null || jwt.isExpired()) {
+            throw new BadCredentialsException("Invalid refresh token.");
+        }
+
+        var user = userRepository.findById(jwt.getUserId()).orElseThrow();
+        return jwtService.generateAccessToken(user);
+    }
 
     public User getCurrentUser() {
         var authentication = SecurityContextHolder.getContext().getAuthentication();
